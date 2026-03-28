2f156c6210d9e2cb64025fdc66bee0d1b8e6edc3
ling <l.ling@Ls-MacBook-Air.local>
2026-03-29 00:09:05 +0700

    lmao

---
+++ hello.py
@@ -0,0 +1,26 @@
+a=1
+b=2
+print (a+b)
+print("Hello,World!")
+print ('hello, world', end ='')
+print ('ts is so ahh')
+print('hello,mtfk, ts so ahhhh')
+print ('hello',222,'missubbi',end='')
+print (2772)
+print(2324)#
+#print(fahhhh)
+"""
+wait why
+"""
+x="whatever bro say"
+print(x)
+y=23
+print(a + y)
+def makecoffee():
+    global a
+    a="gross"
+makecoffee()
+print(a,b)
+print(a[0])
+for z in "banana":
+    print(z)
---
+++ if.py
@@ -0,0 +1,10 @@
+n=24
+if n % 2 == 0:
+    if n in range(2,5):
+        print ("Not Weird")
+    if n in range(6,20):
+        print ("Weird")
+    if n>20:
+        print ("Not Weird")
+else:
+    print("Weird")
